8a9804215457f20dd18fd3d8689feb18b5e1e894
MateusFernan <fernandesmateussilva@gmail.com>
2025-10-26 14:19:42 -0300

    feat: adicionando editoconfig e prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um estudo para aprender a criar um projeto de fora a fora. Implementando o https://www.tabnews.com.br.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Lara gatinha testando </h1>;
+  return <h1>Lara gatinha testando </h1>;
 }
 
 export default Home;
